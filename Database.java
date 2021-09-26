3a2adfb53c5d43f0051e79fa8c458152fb3b6b3e
ttnst02 <91195929+ttnst02@users.noreply.github.com>
2021-09-26 14:49:51 +0700

    food

---
+++ Database.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+public class Database {
+    ArrayList<String> foods = new ArrayList<String>();
+    ArrayList<Integer> ID = new ArrayList<Integer>();
+    ArrayList<String> DATE = new ArrayList<String>();
+    void add(String add)
+    {
+        int id = 1;
+        for(String i : foods)
+            id++;
+        LocalDateTime date = LocalDateTime.now();
+        foods.add(add);
+        ID.add(id);
+        DATE.add(date+"");
+        return;
+    }
+    void scan(String scan)
+    {
+        for(int i=0; i< foods.size() ;i++)
+        {
+            if( foods.get(i).equals(scan))
+            {
+                System.out.println("ID "+ID.get(i)+":  "+foods.get(i)+"  "+DATE.get(i));
+                return;
+            }
+        }
+        System.out.println("This food does not exist");
+        return;
+    }
+    void remove(int remove)
+    {
+        for(int i =remove-1; i<= ID.size()-2;i++)
+        {
+            ID.set(i, ID.get(i+1)-1);
+            foods.set(i, foods.get(i+1));
+            DATE.set(i, DATE.get(i+1));
+        }
+        ID.remove(ID.size()-1);
+        foods.remove(foods.size()-1);
+        DATE.remove(DATE.size()-1);
+    }
+    void printall()
+    {
+        for(int i = 0 ; i < ID.size();i++)
+        {
+            System.out.println("ID "+ID.get(i)+":  "+foods.get(i)+"  "+DATE.get(i));
+        }
+    }
+}
---
+++ Main.java
@@ -0,0 +1,39 @@
+import java.io.*;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Database Database = new Database();
+        int input = 0;
+        String inputstring;
+        int inputint;
+        BufferedReader sb = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Welcome to Food Management - @ 2021 by <Student ID - Student Name >");
+        while(input!=5)
+        {
+            System.out.println("Select the options below: ");
+            System.out.println("1. Add a new food ");
+            System.out.println("2. Search a food by name ");
+            System.out.println("3. Remove the food by ID ");
+            System.out.println("4. Print the food list in the descending order of expired date ");
+            System.out.println("5. Quit ");
+            System.out.print("Enter: ");
+            try
+            {
+                input = Integer.parseInt(sb.readLine());
+                switch(input)
+                {
+                    case 1: System.out.print("Add: "); inputstring = sb.readLine(); Database.add(inputstring); break;
+                    case 2: System.out.print("Scan: "); inputstring = sb.readLine(); Database.scan(inputstring); break;
+                    case 3: System.out.print("Remove id: "); inputint = Integer.parseInt(sb.readLine()); Database.remove(inputint); break;
+                    case 4: Database.printall();
+                }
+            }
+            catch(Exception e)
+            {
+                System.out.println("Error input");
+                continue;
+            }
+        }
+    }
+}
